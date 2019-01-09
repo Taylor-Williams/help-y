084208594ad91c190d729ff38724265e84c16897
Taylor-Williams <taylor@thewilliams.net>
2019-01-08 18:34:55 -0800

    add logout functionality

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,15 @@
 class StaticController < ApplicationController
+  before_action :require_login, only:[:logout]
+
   def login
+    
   end
 
   def logout
+    session.delete(:user_id)
   end
 
   def signup
+  
   end
 end
