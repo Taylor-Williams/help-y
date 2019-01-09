5274d698093865acee691233520854169be2445a
Taylor-Williams <taylor@thewilliams.net>
2019-01-08 17:48:46 -0800

    add User controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,53 @@
 class UsersController < ApplicationController
+    has_secure_password
+    before_action :require_user_authentication, except: [:new, :create]
+
     def new
-        
+      @user = User.new
     end
+  
     def create
-        
+      @user = User.create(user_params)
+      if @user.valid?
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        @errors = @user.errors.messages
+        render :new
+      end
     end
-    def show
-        
-    end
+
     def index
-        
+      @users = Users.new
     end
-    def edit
-        
+  
+    def show
+      @user = User.find(params[:id])
     end
-    def update
-        
-    end
+  
     def destroy
-        
+      User.destroy(params[:id])
+      session.destroy(:user_id)
+      redirect_to root_path
+    end
+  
+    def edit
+      @user = User.find(params[:id])
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+         redirect_to @user
+      else
+         flash[:error] = @user.errors.full_messages.join(", ")
+         render :edit
+      end
+    end
+  
+    private
+  
+    def user_params
+        params.require(:user).permit(:name, :password, :bio, :email, :height, :age)
     end
 end
