3e50654798eb0aebff245a13353defb2c3f0ad95
Taylor-Williams <taylor@thewilliams.net>
2019-01-08 22:28:51 -0800

    remove pry,fix oauth route. move home links to navbar

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -11,9 +11,10 @@ class StaticController < ApplicationController
 
   def logout
     session.delete(:user_id)
+    redirect_to root_path
   end
 
   def signup
-  
+    @user = User.new
   end
 end
