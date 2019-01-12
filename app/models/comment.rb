5cce96564afb68a1352add4b232d20a39683747b
Taylor-Williams <taylor@thewilliams.net>
2019-01-12 00:22:23 -0800

    add comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :post
     belongs_to :user
+
+    validates :user_id, presence: true
+    validates :post_id, presence: true
+    validates :content, length: {in: 1..1000, message: "keep your comment content between 1 and 1000 characters."}
 end
