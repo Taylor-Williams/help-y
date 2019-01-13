d5fb2526e5bbcfcae47afbb4f481e32110d59a89
Taylor-Williams <taylor@thewilliams.net>
2019-01-13 00:24:51 -0800

    add strong params to volunteer controller

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -2,12 +2,12 @@ class VolunteersController < ApplicationController
   before_action :require_login
 
   def manage
-    volunteer = Volunteer.find_by(appointment_id: params[:appointment_id], user_id: params[:user_id])
+    volunteer = Volunteer.find_by(volunteer_params)
     if volunteer
       Volunteer.destroy(volunteer.id)
       flash[:success] = 'successfully un-signed up to volunteer'
     else
-      volunteer = Volunteer.create(appointment_id: params[:appointment_id], user_id: params[:user_id])
+      volunteer = Volunteer.create(volunteer_params)
       if volunteer.valid?
         flash[:success] = 'successfully signed up to volunteer'
       else
@@ -15,5 +15,11 @@ class VolunteersController < ApplicationController
       end
     end
     redirect_to post_path(params[:post_id])
+  end
+
+  private
+  
+  def volunteer_params
+    params.require(:volunteer).permit(:appointment_id, :user_id)
   end
 end
