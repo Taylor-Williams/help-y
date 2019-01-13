357f27e075dea8c7b7229978fa17b4ec879ea3d6
Taylor-Williams <taylor@thewilliams.net>
2019-01-13 00:35:08 -0800

    fix logic in filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,17 @@ class ApplicationController < ActionController::Base
    private
 
    def require_login
-    flash[:danger] = "You must be logged in to access this page"
-    redirect_to root_path unless helpers.logged_in?
+    unless helpers.logged_in?
+      flash[:danger] = "You must be logged in to access this page"
+      redirect_to root_path
+    end
    end
  
    def require_user_authentication
-    flash[:danger] = "You must be logged in to the right account access this page"
-    redirect_to root_path unless helpers.current_user == params[:id].to_i
+    unless helpers.current_user == params[:id].to_i
+      flash[:danger] = "You must be logged in to the right account access this page"
+      redirect_to root_path
+    end
    end
   
 end
