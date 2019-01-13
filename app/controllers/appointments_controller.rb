b471275e0528b8e0a46da0727cdc73c04a01bbc2
Taylor-Williams <taylor@thewilliams.net>
2019-01-12 16:00:15 -0800

    paste everything from comments to appointments for baseline

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,36 @@
 class AppointmentsController < ApplicationController
+  def create
+    if @comment = Comment.create(comment_params)
+      flash[:success] = "Successfully created commment"
+    else
+      flash[:error] = @comment.errors.full_messages.join(", ")
+    end
+    redirect_to post_path(params[:post_id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:success] = "Successfully edited commment"
+    else
+      flash[:error] = @comment.errors.full_messages.join(", ")
+    end
+    redirect_to post_path(params[:post_id])
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment && comment.user_id == session[user_id]
+      Comment.destroy(params[:id])
+      redirect_to post_path(params[:post_id]), success: "Successfully deleted commment"
+    else
+      redirect_to user_path(session[:user_id]), failure: "Cannot delete comment"
+    end
+  end
+
+  private
+
+  def comment_params
+      params.require(:comment).permit(:user_id, :post_id, :content)
+  end
 end
