4f55be26b255621b76dc7d5c0ce6360bb4117bd7
Taylor-Williams <taylor@thewilliams.net>
2019-01-13 00:19:25 -0800

    add flash message to filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
    private
 
    def require_login
-     redirect_to root_path unless helpers.logged_in?
+    flash[:danger] = "You must be logged in to access this page"
+    redirect_to root_path unless helpers.logged_in?
    end
  
    def require_user_authentication
-     redirect_to root_path unless helpers.current_user == params[:id].to_i
+    flash[:danger] = "You must be logged in to the right account access this page"
+    redirect_to root_path unless helpers.current_user == params[:id].to_i
    end
   
 end
