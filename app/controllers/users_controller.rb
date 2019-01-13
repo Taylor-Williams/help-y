2931075294ec343ee19b810e77e6fdffab7f92b0
Taylor-Williams <taylor@thewilliams.net>
2019-01-13 00:26:58 -0800

    pretify users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
         session[:user_id] = @user.id
         redirect_to @user, flash: {success: "Successfully created profile"}
       else
-        redirect_to new_user_path, flash: {error: @user.errors.full_messages.join(", ")}
+        redirect_to new_user_path, flash: {danger: @user.errors.full_messages.join(", ")}
       end
     end
   
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
       if @user.update(user_params)
         redirect_to @user, flash: {success: "Successfully edited profile"}
       else
-        render :show, flash: {error: @user.errors.full_messages.join(", ")}
+        render :show, flash: {danger: @user.errors.full_messages.join(", ")}
       end
     end
 
@@ -33,6 +33,6 @@ class UsersController < ApplicationController
     private
   
     def user_params
-        params.require(:user).permit(:name, :password, :password_confirmation, :bio, :email, :height, :age)
+      params.require(:user).permit(:name, :password, :password_confirmation, :bio, :email, :height, :age)
     end
 end
