a4ab0ff422501cc6fa27cf409392ec8c4cf172b5
Taylor-Williams <taylor@thewilliams.net>
2019-01-12 23:26:17 -0800

    add volunteer/unvolunteer action

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,2 +1,17 @@
 class VolunteersController < ApplicationController
+  def manage
+    volunteer = Volunteer.find_by(appointment_id: params[:appointment_id], user_id: params[:user_id])
+    if volunteer
+      Volunteer.destroy(volunteer.id)
+      flash[:success] = 'successfully un-signed up to volunteer'
+    else
+      volunteer = Volunteer.create(appointment_id: params[:appointment_id], user_id: params[:user_id])
+      if volunteer
+        flash[:success] = 'successfully signed up to volunteer'
+      else
+        flash[:error] = volunteer.errors.full_messages.join(", ")
+      end
+    end
+    redirect_to post_path(params[:post_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   post 'login' => 'static#login', as: "login"
   post 'logout' => 'static#logout', as: "logout"
   get 'signup' => 'static#signup', as: "new_user"
-  get 'auth/github/callback', to: 'sessions#create'
+  get 'auth/github/callback' => 'sessions#create'
   resources :users, except: [:new, :edit, :index]
+  get 'users/new' => 'static#signup'
   resources :posts, except: [:edit] do
     resources :comments, only: [:create, :update, :destroy]
     resources :appointments, only: [:create, :update, :destroy]
   end
-  get 'users/new' => 'static#signup'
+
+  match 'volunteers/:appointment_id/:user_id', to: 'volunteers#manage', via: [:post, :delete], as: 'volunteers'
 end
