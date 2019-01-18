96f1ac325af1b906f100521035b23a0e58363ffe
Taylor-Williams <taylor@thewilliams.net>
2019-01-17 17:28:31 -0800

    separate volunteer controller methods into C,U,D

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,25 +1,40 @@
 class VolunteersController < ApplicationController
   before_action :require_login
 
-  def manage
-    volunteer = Volunteer.find_by(volunteer_params)
-    if volunteer
+  def create
+    volunteer = Volunteer.create(volunteer_params)
+    if volunteer.valid?
+      flash[:success] = 'successfully signed up to volunteer'
+    else
+      flash[:danger] = volunteer.errors.full_messages.join(", ")
+    end
+    redirect_to appointment_path(params[:appointment_id])
+  end
+
+  def update
+    volunteer = Volunteer.find(params[:id])
+    if volunteer && volunteer.valid?
+      flash[:success] = 'successfully edited volunteer dates'
+    else
+      flash[:danger] = volunteer.errors.full_messages.join(", ")
+    end
+    redirect_to appointment_path(params[:appointment_id])
+  end
+
+  def destroy
+    volunteer = Volunteer.find(params[:id])
+    if volunteer && helpers.is_current_user?(volunteer.user)
       Volunteer.destroy(volunteer.id)
       flash[:success] = 'successfully un-signed up to volunteer'
     else
-      volunteer = Volunteer.create(volunteer_params)
-      if volunteer.valid?
-        flash[:success] = 'successfully signed up to volunteer'
-      else
-        flash[:danger] = volunteer.errors.full_messages.join(", ")
-      end
+      flash[:danger] = volunteer.errors.full_messages.join(", ")
     end
-    redirect_to post_path(params[:post_id])
+    redirect_to appointment_path(params[:appointment_id])
   end
 
   private
   
   def volunteer_params
-    params.permit(:appointment_id, :user_id)
+    params.require(:volunteer).permit(:start_date, :end_date, :appointment_id, :user_id)
   end
 end
