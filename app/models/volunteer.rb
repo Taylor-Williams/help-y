4804c64cc71c102b55bee118623637fd165f6049
Taylor-Williams <taylor@thewilliams.net>
2019-01-17 17:29:45 -0800

    add validations to appointments, volunteers models

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,12 @@
 class Volunteer < ApplicationRecord
   belongs_to :user
-  belongs_to :appointment
+  belongs_to :appointment, inverse_of: :volunteers
+
+  validates :start_date, date: { after_or_equal_to: Proc.new { |obj| obj.appointment.start_date },
+    after_or_equal_to: Proc.new { Date.today },
+    message: 'must be after today and appointment start date' }, on: :create
+
+  validates :end_date, date: { after: :start_date,
+    before_or_equal_to: Proc.new { |obj| obj.appointment.end_date },
+    message: 'must be after start date and before appointment end date' }
 end
