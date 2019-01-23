105a48c3a0b35125261fb0fe78f7cc3ce0136529
Taylor-Williams <taylor@thewilliams.net>
2019-01-22 23:33:47 -0800

    update log in message and spec.md

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
-    redirect_to @user, flash: {success: "Successfully created profile"}
+    redirect_to @user, flash: {success: "Successfully logged in"}
   end
 
   def login
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       @user = user
       session[:user_id] = @user.id
-      redirect_to @user, flash: {success: "You are now logged in"}
+      redirect_to @user, flash: {success: "Successfully logged in"}
     else
       redirect_to root_path, flash: {danger: "email and/or password was incorrect"}
     end
