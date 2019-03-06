4b65a1f8fb2572e1cbaf5be58648518ba85601b6
Taylor-Williams <taylor@thewilliams.net>
2019-03-05 20:15:30 -0800

    mostly working version of Comment template

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,19 +1,32 @@
 class Comment {
-  constructor(attributes){
+  constructor(attributes) {
     this.user = attributes.user
     this.content = attributes.content
-    this.post = attributes.post
-  }  
-} 
-var source   = document.getElementById("entry-template").innerHTML;
-var template = Handlebars.compile(source);
+    this.post = attributes.post 
+  }
+}
+$(
+  () => {
+    Comment.source = document.getElementById("comment-template").innerHTML;
+    Comment.template = Handlebars.compile(Comment.source);
+    console.log(Comment.template())
+  }
+)
+Comment.prototype.renderComment = () => {
+  return Comment.template(this)
+}
 $(
   () => {
     $('.getComments').on("submit", (e) => {
       e.preventDefault()
       address = $('.getComments').attr("action")
       $.get(address, (comments) => {
-        comments.forEach((comment) => {$(".comments").append(comment.content)})
+        console.log(comments)
+        comments.forEach((comment) => {
+          c = new Comment(comment)
+          console.log(c)
+          console.log(c.renderComment())
+          $(".comments").append(c.renderComment())})
       })
     })
   }
