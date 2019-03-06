25428f6597ce83144f674e89149b7097bd28321c
Taylor-Williams <taylor@thewilliams.net>
2019-03-05 23:01:08 -0800

    add semi-working new Comment button

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,6 +2,7 @@ class Comment {
   constructor(attributes) {
     this.user = attributes.user
     this.content = attributes.content
+    this.id = attributes.id
     this.post = attributes.post
     this.updated_at = attributes.updated_at
   }
@@ -10,7 +11,7 @@ class Comment {
   }
 }
 Comment.getComments = function() {
-  address = $('.getComments').attr("action")
+  address = $('.get-comments').attr("action")
   $.get(address, (comments) => {
     if(comments.length) {
       comments.forEach((comment) => {
@@ -25,7 +26,12 @@ $(
   function() {
     Comment.source = document.getElementById("comment-template").innerHTML
     Comment.template = Handlebars.compile(Comment.source)
-    $('.getComments').on("submit", function(e){
+    Comment.commentFormSource = document.getElementById("comment-form-template").innerHTML
+    Comment.commentFormTemplate = Handlebars.compile(Comment.commentFormSource)
+    console.log(Comment.commentFormTemplate())
+    Comment.userID = $(".user-link").attr("href").slice(-1)
+    $('#new-comment').text(Comment.commentFormTemplate(new Comment({user: Comment.userID})))
+    $('.get-comments').on("submit", function(e){
       e.preventDefault()
       Comment.getComments()
     })
