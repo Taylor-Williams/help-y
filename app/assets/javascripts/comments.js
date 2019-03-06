20ed56fc56c9ca1a35e6d46145f0de13d7465a92
Taylor-Williams <taylor@thewilliams.net>
2019-03-05 21:09:36 -0800

    refactor Comment class

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -5,30 +5,29 @@ class Comment {
     this.post = attributes.post
     this.updated_at = attributes.updated_at
   }
+  renderComment() {
+    return Comment.template(this)
+  }
+}
+Comment.getComments = function() {
+  address = $('.getComments').attr("action")
+  $.get(address, (comments) => {
+    if(comments.length) {
+      comments.forEach((comment) => {
+      c = new Comment(comment)
+      $(".comments").append(c.renderComment())})
+    } else {
+      $(".comments").text("There are no comments for this post")
+    }
+  })
 }
 $(
-  () => {
+  function() {
     Comment.source = document.getElementById("comment-template").innerHTML
     Comment.template = Handlebars.compile(Comment.source)
-  }
-)
-Comment.prototype.renderComment = function() {
-  return Comment.template(this)
-}
-$(
-  () => {
-    $('.getComments').on("submit", (e) => {
+    $('.getComments').on("submit", function(e){
       e.preventDefault()
-      address = $('.getComments').attr("action")
-      $.get(address, (comments) => {
-        if(comments.length) {
-          comments.forEach((comment) => {
-          c = new Comment(comment)
-          $(".comments").append(c.renderComment())})
-        } else {
-          $(".comments").text("There are no comments for this post")
-        }
-      })
+      Comment.getComments()
     })
   }
 )
