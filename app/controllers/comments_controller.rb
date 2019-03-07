fc7fcec8264e78f8c82865db6659a5ac9ee57036
Taylor-Williams <taylor@thewilliams.net>
2019-03-06 21:03:59 -0800

    applied filter logic to all comment controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,55 +6,34 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      respond_to do |f|
-        f.json {render json: @comment, status: 200}
-        f.html {redirect_to post_path(params[:post_id])}
-      end
+      render json: @comment, status: 200
     else
-      respond_to do |f|
-        f.json {render json: {response: 'invalid comment'}, status: 400}
-        f.html {redirect_to post_path(params[:post_id])}
-      end
+      render json: {response: 'invalid comment'}, status: 400
     end
   end
 
   def show
-    post = Post.find(params[:post_id])
-    comment = Comment.find(params[:id])
-    if post && comment
-      render json: comment, status: 200
-    else
-      render json: {response: 'invalid Post id'}, status: 400
-    end
+    render json: @comment, status: 200
   end
 
   def index
-    post = Post.find(params[:post_id])
-    if post
-      render json: post.comments, status: 200
-    else
-      render json: {response: 'invalid Post id'}, status: 400
-    end
+    render json: @post.comments, status: 200
   end
 
   def update
-    if Post.find(params[:post_id])
-      @comment = Comment.find(params[:id])
-      if @comment.update(comment_params)
-        render json: @comment, status: 200
-      else
-        render json: {response: 'invalid comment'}, status: 400
-      end
+    if @comment.update(comment_params)
+      render json: @comment, status: 200
+    else
+      render json: {response: 'invalid comment params'}, status: 400
     end
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    if comment && comment.user_id == session[:user_id]
+    if @comment.user_id == session[:user_id]
       Comment.destroy(params[:id])
-      redirect_to post_path(params[:post_id]), flash: {success: "Successfully deleted comment"}
+      render status: 200
     else
-      redirect_to user_path(session[:user_id]), flash: {danger: "Cannot delete comment"}
+      render json: {response: 'invalid user'}, status: 400
     end
   end
 
@@ -65,10 +44,12 @@ class CommentsController < ApplicationController
   end
   
   def find_post
-    flash[danger: "not a valid post"] unless post = Post.find(params[:post_id]) 
+    @post = Post.find(params[:post_id])
+    flash[danger: "not a valid post"] unless @post
   end
 
   def find_comment
-    flash[danger: "not a valid comment"] unless comment = Comment.find(params[:id])
+    @comment = Comment.find(params[:id])
+    flash[danger: "not a valid comment"] unless @comment
   end
 end
