2b043050a196e2b1d2ea4d217a4fc3fe0f92d88f
Taylor-Williams <taylor@thewilliams.net>
2019-03-07 23:55:36 -0800

    blindly copy some comments.js into available_appts.js

---
+++ app/assets/javascripts/available_appointments.js
@@ -0,0 +1,29 @@
+class Comment {
+  constructor(attributes = {}) {
+    this.user = attributes.user
+    this.content = attributes.content
+    this.id = attributes.id
+    this.post = attributes.post
+    this.updated_at = attributes.updated_at
+    this.isCurrentUser = (this.user && this.user.id && parseInt(this.user.id) === parseInt(Comment.userID))
+  }
+  renderComment() {
+    return Comment.template(this)
+  }
+  renderForm(){
+    return Comment.formTemplate(this)
+  }
+}
+$(
+  function() {
+    Comment.renderTemplates()
+    Comment.renderAttributes()
+    Comment.newCommentButton.on("click", function(){
+      Comment.newCommentForm()
+    })
+    Comment.getCommentsForm.on("submit", function(e){
+      e.preventDefault()
+      Comment.getComments()
+    })
+  }
+)
