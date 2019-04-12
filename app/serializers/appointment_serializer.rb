3223824e2ed1f9f6f8cdc934f86e01617ef7477d
Taylor-Williams <taylor@thewilliams.net>
2019-04-11 22:19:30 -0700

    add owner_name to appointments serializer

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,4 +1,4 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :end_date, :info, :title, :volunteers_count, :owner_id
+  attributes :id, :start_date, :end_date, :info, :title, :volunteers_count, :owner_id, :owner_name
   belongs_to :post
 end
