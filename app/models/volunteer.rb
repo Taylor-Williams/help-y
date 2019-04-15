042a17886fdbb86745556aa9ffb08ccf5ab29d41
Taylor-Williams <taylor@thewilliams.net>
2019-04-15 13:21:05 -0700

    update validations for volunteering to opportunities

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -4,9 +4,9 @@ class Volunteer < ApplicationRecord
 
   validates :start_date, date: { after_or_equal_to: Proc.new { |obj| obj.appointment.start_date },
     after_or_equal_to: Proc.new { Date.today },
-    message: 'must be after today and appointment start date' }, on: :create
+    message: 'can\'t start before today, must be after appointment start date' }, on: :create
 
-  validates :end_date, date: { after: :start_date,
+  validates :end_date, date: { after_or_equal_to: :start_date,
     before_or_equal_to: Proc.new { |obj| obj.appointment.end_date },
     message: 'must be after start date and before appointment end date' }
 end
