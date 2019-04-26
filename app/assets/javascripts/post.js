55709a24ce1d532626472fa250ef48b660e74a86
Taylor-Williams <taylor@thewilliams.net>
2019-04-25 19:54:31 -0700

    add simple functionality for rendering comments under post in posts/index

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,26 +1,30 @@
-class Post{
-  constructor(attributes = {}){
-    this.comments = comments
-  }
-}
+class Post{}
 Post.getPostRequest = function(){
   $.ajax({
     dataType: "JSON",
-    url: `/posts/{this.dataset.id}`,
+    url: `/posts/${this.dataset.id}`,
     method: "get"
-  })
+  }).success(Post.renderAllComments)
+  .fail(() => {console.log("there was some error that happened.")})
 }
 Post.renderAllComments = function(post){
+  console.log(post)
+  $postCommentsDiv = $(`#${post.id}-comments`)
+  $postCommentsDiv.empty()
   post.comments.forEach((comment) => {
-
-  }
+    console.log(comment)
+    $postCommentsDiv.append(Post.commentTemplate(comment))
+  })
 }
 Post.addCommentsListener = function() {
   $('.get-post-comments').click(this.getPostRequest)
-  .success(this.renderAllComments)
 }
 Post.ready = function(){
-  Post.addCommentsListener()
+  this.addCommentsListener()
+  this.commentTemplate = Handlebars.compile(document.getElementById("comment-template").innerHTML)
+}
+Post.renderComment = function(comment) {
+  return Post.commentTemplate(comment)
 }
 $(document).on("turbolinks:load", function() {
   Post.ready()
